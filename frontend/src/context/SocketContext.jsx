ce074685ba74a0ed2119e0e3fb3fe83e78b72306
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2024-07-20 01:18:30 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,6 +14,44 @@ export const SocketContextProvider = ({ children }) => {
   const [onlineUsers, setOnlineUsers] = useState([]);
   const user = useRecoilValue(userAtom);
 
+  useEffect(() => {
+    const socket = io("http://localhost:7500", {
+      query: {
+        userId: user?._id,
+      },
+    });
+    setSocket(socket);
+
+    socket.on("getOnlineUsers", (users) => {
+      setOnlineUsers(users);
+    });
+
+    return () => socket && socket.close();
+  }, [user?._id]);
+
+  console.log(onlineUsers, "Online users");
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+import { createContext, useContext, useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import io from "socket.io-client";
+import userAtom from "../atoms/userAtom";
+
+const SocketContext = createContext();
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const user = useRecoilValue(userAtom);
+
  useEffect(() => {
   if (user?._id) {
     const socket = io("https://socialmedia-tf66.onrender.com", {
